6c2fde1fab1472c5b96d14458c6e19a18aec8d2f
Nazar Chaban <134311145+Vivern0@users.noreply.github.com>
2024-04-25 21:57:28 +0300

    origins

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import uvicorn
 
@@ -8,6 +9,18 @@ app = FastAPI()
 app.include_router(auth.router, prefix='/api')
 app.include_router(users.router, prefix='/api')
 app.include_router(contacts.router, prefix="/api")
+
+origins = [
+    'http://contact-manager.com/frontend',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/')
